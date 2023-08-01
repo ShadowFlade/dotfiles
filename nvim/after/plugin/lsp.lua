2bd9960810f972194d9993f3278b5908dba198d3
ShadowFlade <wolffdrugov@yandex.ru>
2023-08-01 23:52:51 +0300

    master:nvim:fuck that linter

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -97,28 +97,28 @@ lspConfig.diagnosticls.setup {
   on_attach = on_attach,
   filetypes = { 'javascript', 'javascriptreact', 'json', 'typescript', 'typescriptreact', 'css', 'less', 'scss', 'markdown', 'pandoc' },
   init_options = {
-    linters = {
-      eslint = {
-        command = 'eslint_d',
-        rootPatterns = { '.git' },
-        debounce = 100,
-        args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
-        sourceName = 'eslint_d',
-        parseJson = {
-          errorsRoot = '[0].messages',
-          line = 'line',
-          column = 'column',
-          endLine = 'endLine',
-          endColumn = 'endColumn',
-          message = '[eslint] ${message} [${ruleId}]',
-          security = 'severity'
-        },
-        securities = {
-          [2] = 'error',
-          [1] = 'warning'
-        }
-      },
-    },
+    --linters = {
+    --  eslint = {
+    --    command = 'eslint_d',
+    --    rootPatterns = { '.git' },
+    --    debounce = 100,
+    --    args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
+    --    sourceName = 'eslint_d',
+    --    parseJson = {
+    --      errorsRoot = '[0].messages',
+    --      line = 'line',
+    --      column = 'column',
+    --      endLine = 'endLine',
+    --      endColumn = 'endColumn',
+    --      message = '[eslint] ${message} [${ruleId}]',
+    --      security = 'severity'
+    --    },
+    --    securities = {
+    --      [2] = 'error',
+    --      [1] = 'warning'
+    --    }
+    --  },
+    --},
     filetypes = {
       javascript = 'eslint',
       javascriptreact = 'eslint',
@@ -126,11 +126,11 @@ lspConfig.diagnosticls.setup {
       typescriptreact = 'eslint',
     },
     formatters = {
-      eslint_d = {
-        command = 'eslint_d',
-        args = { '--stdin', '--stdin-filename', '%filename', '--fix-to-stdout' },
-        rootPatterns = { '.git' },
-      },
+  --    eslint_d = {
+  --      command = 'eslint_d',
+  --      args = { '--stdin', '--stdin-filename', '%filename', '--fix-to-stdout' },
+  --      rootPatterns = { '.git' },
+  --    },
       prettier = {
         command = 'prettier',
         args = { '--stdin-filepath', '%filename' }
@@ -138,8 +138,8 @@ lspConfig.diagnosticls.setup {
     },
     formatFiletypes = {
       css = 'prettier',
-      javascript = 'eslint_d',
-      javascriptreact = 'eslint_d',
+ --     javascript = 'eslint_d',
+--      javascriptreact = 'eslint_d',
       scss = 'prettier',
       less = 'prettier',
       typescript = 'eslint_d',
--- nvim/lua/shadowflade/set.lua
+++ nvim/lua/shadowflade/set.lua
@@ -31,4 +31,4 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
-
+vim.g.textWidth = 80;
