a2204a0f5d4b679289a2b91de21bc4bc6f5b84b0
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-06-15 00:04:51 +0300

    master:snippets for nvim

--- nvim/lua/shadowflade/init.lua
+++ nvim/lua/shadowflade/init.lua
@@ -8,31 +8,16 @@ local ShadowFladeGroup = augroup('ShadowFlade', {})
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
 vim.filetype.add({
     extension = {
         templ = 'templ',
     }
 })
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
-autocmd('LspAttach', {
-    group = ShadowFladeGroup,
-    callback = function(ev)
-        local opts = { buffer = ev.buf }
-        vim.keymap.set("n","gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n","K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n","<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n","<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n","[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n","]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n","<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n","<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n","<leader>vrn", function() vim.lsp.buf.rename()() end, opts)
-        vim.keymap.set("n","<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
-    end,
-})
 autocmd('TextYankPost', {
     group = yank_group,
     pattern = '*',
@@ -44,14 +29,30 @@ autocmd('TextYankPost', {
     end,
 })
 
-
 autocmd({"BufWritePre"}, {
     group = ShadowFladeGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
 
+autocmd('LspAttach', {
+    group = ShadowFladeGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
--- nvim/lua/shadowflade/lazy/lsp.lua
+++ nvim/lua/shadowflade/lazy/lsp.lua
@@ -1,15 +1,3 @@
-local function attach(opts)
-    vim.keymap.set("n","gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n","K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n","<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n","<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n","[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n","]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n","<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n","<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n","<leader>vrn", function() vim.lsp.buf.rename()() end, opts)
-    vim.keymap.set("n","<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -45,7 +33,6 @@ return {
                 function(server_name) -- default handler (optional)
 
                     require("lspconfig")[server_name].setup {
-                        on_attach = attach,
                         capabilities = capabilities
                     }
                 end,
